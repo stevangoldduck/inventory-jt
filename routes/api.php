20cb7379f305ae87be5bf5e8d0306e4345117b4d
stevangoldduck <stevanlai04@gmail.com>
2020-02-18 01:08:44 +0700

    Auth API

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Http\Request;
 
 include('api_routes/auth_routes.php');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('getUser','API\AuthController@getCurrentUser');
 });
