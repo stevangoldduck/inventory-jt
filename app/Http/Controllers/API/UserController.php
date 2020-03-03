abe84cb9841c9390e51f3e6144516745780524b0
stevangoldduck <stevanlai04@gmail.com>
2020-03-03 07:22:44 +0700

    User Api Resource

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Validator;
+use phpseclib\Crypt\Hash;
 
 class UserController extends Controller
 {
@@ -15,7 +16,13 @@ class UserController extends Controller
     public function getUser()
     {
         $data = User::all();
-        return response()->json(['list_user' => $data], 200);
+        return response()->json(['list_user' => $data], $this->successStatus);
+    }
+
+    public function searchUser($id)
+    {
+        $data = User::find($id);
+        return response()->json($data, $this->successStatus);
     }
 
     public function postUser(Request $req)
@@ -29,7 +36,49 @@ class UserController extends Controller
 
         if($validate->fails())
         {
-            return response()->json($validate->messages(), 200);
+            return response()->json($validate->messages(), $this->successStatus);
         }
+
+        $user = new User();
+        $user->name = $req->name;
+        $user->email = $req->email;
+        $user->password = Hash::make($req->password);
+        $user->role = $req->role;
+        $user->save();
+
+        return response()->json(['message' => 'User added'],$this->successStatus);
+    }
+
+    public function deleteUser(Request $req)
+    {
+        $data = User::find($req->id);
+        $data->delete();
+
+        return response()->json(['message' => 'User deleted'], $this->successStatus);
+    }
+
+    public function updateUser(Request $req)
+    {
+        $validate = Validator::make($req->all(),
+        [
+            'name' => 'required',
+            'email' => 'required|unique:users,'.$req->id
+        ]);
+
+        if($validate->fails())
+        {
+            return response()->json($validate->messages(), $this->successStatus);
+        }
+
+        $user = User::find($req->id);
+        $user->name = $req->name;
+        $user->email = $req->email;
+        if($req->password != null || $req->password != '')
+        {
+            $user->password = Hash::make($req->password);
+        }
+        $user->save();
+
+        return response()->json(['message' => 'User updated'],$this->successStatus);
     }
 }
