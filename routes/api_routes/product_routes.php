0a153c22b53f9f1e901a36b846113617d2122dc3
stevangoldduck <stevanlai04@gmail.com>
2020-03-25 15:11:26 +0700

    product routes

--- routes/api_routes/product_routes.php
+++ routes/api_routes/product_routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('get-product','API\ProductController@index');
-Route::post('add-product','API\ProductController@store');
-Route::put('update-product','API\ProductController@update');
-Route::delete('delete-product/{id}','API\ProductController@destroy');
+Route::get('get-product','API\ProductController@index')->name('product.get');
+Route::post('add-product','API\ProductController@store')->name('product.store');
+Route::put('update-product','API\ProductController@update')->name('product.update');
+Route::delete('delete-product/{id}','API\ProductController@destroy')->name('product.delete');
